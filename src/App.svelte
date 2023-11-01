4e7b2bf4b06ce7b31c425aeecf0568adb531a920
Javier Cavedal <jcavedal@gmail.com>
2023-11-01 17:10:35 -0300

    Creados componente Person y Text

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,50 @@
 <script>
+	import About from "./components/About.svelte";
+	import Text from "./components/Text.svelte";
+	import Person from "./components/Person.svelte";
 	export let name;
 	export let lastName;
+	export let age;
+	let svelteLogo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsbE4nJzFo9qHn8wMCj8Vlr3qv9dWa2Hm0XA&usqp=CAU";
+
+	const data = {
+		name: "Javier",
+		lastName: "Cavedal",
+		age: 24
+	}
+
 </script> 
 
 <main>
-	<h1>Hello {name} {lastName}!</h1>
+	<h1>Hello {data.name} {data.lastName}!</h1>
+	<About />
+	<Text anotherText="Hola!"/>
+	<Text />
+	<Person {...data} />
+	<img src={svelteLogo} alt="Svelte">
 </main>
 
 <style>
+
+	:global(body) {
+		background-color: #f2eee2;
+		color: #0084f6;
+	}
+
+	:global(root) {
+		--theme-color: purple;
+		font-size: 22px;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	main img {
+		width:20%;
 	}
 
 	h1 {
---
+++ src/components/About.svelte
@@ -0,0 +1,20 @@
+<script>
+    let variable = 'HOLA MUNDO';
+    let count = 0;
+
+    function clickCount() {
+        count += 1;
+    }
+
+</script>
+
+<style>
+    p {
+        color: var(--theme-color);
+    }
+</style>
+
+<div class="About">
+    <p>{variable}</p>
+    <button on:click={clickCount}>Click {count === 0 ? '' : count}</button>
+</div>
